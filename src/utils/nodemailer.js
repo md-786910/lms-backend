0b17b14e07b1dab4e60162c8abdb87f9e03aa9e3
md-786910 <mdashifreza111@gmail.com>
2025-11-13 19:10:34 +0530

    Fxied nodemailer issyes!

--- src/utils/nodemailer.js
+++ src/utils/nodemailer.js
@@ -6,16 +6,18 @@ try {
   transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST, // e.g., smtp.gmail.com
     port: process.env.SMTP_PORT, // 465 (SSL) or 587 (TLS)
-    secure: false, // true for 465, false for 587
+    secure: true, // true for 465, false for 587
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
     },
+    debug: true,
   });
 
   // Verify connection
   transporter.verify((error, success) => {
     if (error) {
+      console.log({ error });
       console.error("SMTP Connection failed:", error.message);
     } else {
       console.log("SMTP Connected:", success);
